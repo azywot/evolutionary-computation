aed2d72510045a80852047e785899cd8101c2d38
Zuza Gawrysiak <zuzanna.gawrysiak@fourpoint.space>
2023-10-10 13:17:32 +0200

    Calculate distance matrix

--- src/parse.jl
+++ src/parse.jl
@@ -1,4 +1,10 @@
-using CSV, DataFrames
+using CSV, DataFrames, Distances
 
 df = CSV.read("evolutionary-computation/data/TSPA.csv", DataFrame, header=false)
 rename!(df,[:x,:y,:cost])
+
+coords = reshape(Array(df[!, Not("cost")]), (2, 200))
+
+# we need only these two variables
+distance_matrix = round.(Int, pairwise(Euclidean(), coords))
+cost_vector = df[!, "cost"]
