0efeca1adac5b7c93146150f8558140d70bc861a
azywot <agata.zywot@gmail.com>
2023-10-24 09:15:13 +0200

    change intra/inter route

--- src/methods/local_greedy_search.jl
+++ src/methods/local_greedy_search.jl
@@ -4,23 +4,14 @@ using Random
 Generate an intra route solution.
 - `solution::Vector{Int}`: solution
 - `distance_matrix::Matrix{Int}`: matrix of distances between nodes
+- `indices::Vector{Int}`: indices of nodes to be swapped (either the nodes or its edges)
 - `mode::String`: mode of the local search, either "node" or "edge"
 
 returns: a local search solution and its delta
 """
-function choose_intra_route_move(solution, distance_matrix, mode)
+function choose_intra_route_move(solution, distance_matrix, indices, mode)
     n = length(solution)
     solution = deepcopy(solution)
-
-    indices = []
-    while length(indices) < 2
-        idx1 = rand(1:length(solution))
-        idx2 = rand(1:length(solution))
-        if idx1 < idx2
-            push!(indices, idx1)
-            push!(indices, idx2)
-        end
-    end
     idx1, idx2 = indices[1], indices[2]
 
     if mode == "node"
@@ -35,10 +26,12 @@ function choose_intra_route_move(solution, distance_matrix, mode)
         delta = - negative_ingredient_node_1 - negative_ingredient_node_2 + positive_ingredient_node_1 + positive_ingredient_node_2
         
         return solution, delta
+
     else # edge mode
         # based on slide 93
         solution = solution[1:idx1] + reverse(solution[idx1+1:idx2]) + solution[mod(idx2+1, n)+1:end]
 
+        # calculate just delta
         negative_ingredient = distance_matrix[solution[idx1], solution[idx+1]] + distance_matrix[solution[idx2], solution[mod(idx2+1, n)+1]]
         positive_ingredient = distance_matrix[solution[idx1], solution[idx2]] + distance_matrix[solution[idx+1], solution[mod(idx2+1, n)+1]]
 
@@ -52,20 +45,18 @@ Generate an inter route solution.
 - `solution::Vector{Int}`: solution
 - `distance_matrix::Matrix{Int}`: matrix of distances between nodes
 - `cost_vector::Vector{Int}`: vector of costs of node
+- `indices::Vector{Int}`: indices of nodes to be swapped
 
 returns: a local search solution and its delta
 """
-function choose_inter_route_move(solution, distance_matrix, cost_vector)
+function choose_inter_route_move(solution, distance_matrix, cost_vector, indices)
     n = length(solution)
     solution = deepcopy(solution)
-    N, _ = size(distance_matrix)
-    unvisited = setdiff(Set(1:N), Set(solution))
-    unvisited = collect(unvisited)
 
-    candidate_idx = rand(1:length(unvisited))
+    candidate_idx = indices[1]
     candidate = unvisited[candidate_idx]
 
-    substitute_idx = rand(1:length(solution))
+    substitute_idx = indices[2]
     substitute = solution[substitute_idx]
     solution[substitute_idx] = candidate
 
@@ -80,7 +71,7 @@ end
 
 
 """
-Generate a local search solution given a starting solution and a mode.
+Generate a local search greedy solution given a starting solution and a mode.
 - `iterations::Int`: number of iterations
 - `solution::Vector{Int}`: initial solution
 - `distance_matrix::Matrix{Int}`: matrix of distances between nodes
@@ -90,8 +81,8 @@ Generate a local search solution given a starting solution and a mode.
 returns: a local search solution and its cost
 """
 function local_greedy_search(iterations, solution, distance_matrix, cost_vector, mode)
-
-    distance_matrix = deepcopy(distance_matrix) 
+    N, _ = size(distance_matrix)
+    distance_matrix = deepcopy(distance_matrix) # fix diagonal as now its 10000000 or sth
     cost_vector = deepcopy(cost_vector)
     best_solution = deepcopy(solution)
     best_cost = evaluate_solution(best_solution, distance_matrix, cost_vector)
@@ -106,9 +97,26 @@ function local_greedy_search(iterations, solution, distance_matrix, cost_vector,
             p = rand()
             println("p: ", p)
             if p < 0.5
-                new_solution, delta = choose_intra_route_move(best_solution, distance_matrix, mode)
+                # intra-route; change edge or node withing best_solution
+                indices = []
+                while length(indices) < 2
+                    idx1 = rand(1:length(solution))
+                    idx2 = rand(1:length(solution))
+                    if idx1 < idx2
+                        push!(indices, idx1)
+                        push!(indices, idx2)
+                    end
+                end
+                new_solution, delta = choose_intra_route_move(best_solution, distance_matrix, indices, mode)
             else
-                new_solution, delta = choose_inter_route_move(best_solution, distance_matrix, cost_vector)
+                # inter-route; change node from best_solution with a node from unvisited
+                unvisited = setdiff(Set(1:N), Set(solution))
+                unvisited = collect(unvisited)
+
+                candidate_idx = rand(1:length(unvisited))
+                substitute_idx = rand(1:length(solution))
+                indices = [candidate_idx, substitute_idx]
+                new_solution, delta = choose_inter_route_move(best_solution, distance_matrix, cost_vector, indices)
             end
 
             if delta < 0
@@ -120,4 +128,21 @@ function local_greedy_search(iterations, solution, distance_matrix, cost_vector,
         println("Best cost: ", best_cost, " delta: ", delta)
     end
     return best_solution, best_cost
+end
+
+
+
+"""
+Generate a local search steepest solution given a starting solution and a mode.
+- `iterations::Int`: number of iterations
+- `solution::Vector{Int}`: initial solution
+- `distance_matrix::Matrix{Int}`: matrix of distances between nodes
+- `cost_vector::Vector{Int}`: vector of costs of node
+- `mode::String`: mode of the local search, either "node" or "edge"
+
+returns: a local search solution and its cost
+"""
+function local_steepest_search()
+    # TODO: by analogy with local_greedy_search
+    ...
 end
